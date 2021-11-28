af7d6ed5cfed1d89b15c34a239c3838b4a1e0462
Prakshal Pro <prakshal@Prakshals-MacBook-Pro.local>
2021-11-28 13:33:09 -0500

    fixed styling

--- experiences.js
+++ experiences.js
@@ -1,7 +1,7 @@
 // Enter JSON filename as json_url
 json_url = "experiences.json"
 
-function get_data(){
+function get_data() {
     var parsed_json = [];
     $.ajax({
         'async': false,
@@ -17,11 +17,6 @@ function get_data(){
 
 var all_experiences = get_data();
 
-for(let exp of all_experiences){
-    if(exp["duration"][1] == "Present"){
-        document.getElementById("experiences").innerHTML += '<div class="timeline-container primary"><div class="timeline-icon"><i class="fa fa-calendar-plus-o"></i></div><div class="timeline-body"><h4 class="timeline-title"><span class="badge">'+exp["place"]+'</span></h4><h3>'+exp["title"]+'</h3><p class="timeline-subtitle">'+exp["duration"][0]+' - '+exp["duration"][1]+'</p></div>'
-    }
-    else{
-        document.getElementById("experiences").innerHTML += '<div class="timeline-container primary"><div class="timeline-icon"><i class="fa fa-calendar-check-o"></i></div><div class="timeline-body"><h4 class="timeline-title"><span class="badge">'+exp["place"]+'</span></h4><h3>'+exp["title"]+'</h3><p class="timeline-subtitle">'+exp["duration"][0]+' - '+exp["duration"][1]+'</p></div>'
-    }
+for (let exp of all_experiences) {
+    document.getElementById("experiences").innerHTML += '<div class="timeline-container primary"><div class="timeline-icon"></div><div class="timeline-body"><h4 class="timeline-title"><span class="badge">' + exp["place"] + '</span></h4><h3>' + exp["title"] + '</h3><p class="timeline-subtitle">' + exp["duration"][0] + ' - ' + exp["duration"][1] + '</p></div>'
 }
